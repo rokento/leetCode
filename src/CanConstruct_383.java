79635fafb0ad107c26060251911ce3fd5e92cc9c
Luxiandong <1473456200@qq.com>
2023-08-14 19:31:33 +0800

    383. 赎金信

---
+++ src/CanConstruct_383.java
@@ -0,0 +1,67 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 383. 赎金信
+ * 给你两个字符串：ransomNote 和 magazine ，判断 ransomNote 能不能由 magazine 里面的字符构成。
+ *
+ * 如果可以，返回 true ；否则返回 false 。
+ *
+ * magazine 中的每个字符只能在 ransomNote 中使用一次。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：ransomNote = "a", magazine = "b"
+ * 输出：false
+ * 示例 2：
+ *
+ * 输入：ransomNote = "aa", magazine = "ab"
+ * 输出：false
+ * 示例 3：
+ *
+ * 输入：ransomNote = "aa", magazine = "aab"
+ * 输出：true
+ *
+ *
+ * 提示：
+ *
+ * 1 <= ransomNote.length, magazine.length <= 105
+ * ransomNote 和 magazine 由小写英文字母组成
+ */
+public class CanConstruct_383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for(char c : ransomNote.toCharArray()) {
+            if (!map.containsKey(c) || map.get(c) == 0) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+        }
+        return true;
+    }
+
+    /**
+     * 解法2：  使用ASCII码
+     */
+    public boolean canConstruct2(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        int[] cnt = new int[26];
+        for (char c : magazine.toCharArray()) {
+            cnt[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()) {
+            cnt[c - 'a']--;
+            if(cnt[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
